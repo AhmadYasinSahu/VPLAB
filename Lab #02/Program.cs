21f9b95be3e3f18eaea5715e328114438ef9574c
AhmadYasinSahu <233605@students.au.edu.pk>
2024-10-23 12:00:42 +0500

    Add files via upload

---
+++ Lab #02/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+
+            //Console.WriteLine("  Ahmad Yasin  ");
+            //Console.WriteLine(" 233605 ");
+            //Console.WriteLine("Nishtar Employee Colony,Multan.");
+
+
+                Console.WriteLine(args.Length);
+
+                Console.WriteLine(args[0]);
+                Console.WriteLine(args[1]);
+                Console.WriteLine(args[2]);
+                  
+           Console.ReadLine();
+    }
+}
+}
