1dadf2a191eea4bfb1bae2357eabe98f5cb1e0b8
AhmadYasinSahu <233605@students.au.edu.pk>
2024-11-20 13:59:44 +0500

    Add files via upload

---
+++ Lab #08/Activity 01/Program.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+using System.Data;
+using System.Data.OleDb;
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + "c:\\Data\\Northwind.mdb;User Id=admin;Password=;";
+        string queryString = "SELECT ProductID, UnitPrice, ProductName from products " + "WHERE UnitPrice > ? " + "ORDER BY UnitPrice DESC;";
+        int paramValue = 5;
+
+        Ahmad connection = new Ahmad(connectionString);
+        Ahmad command = new Ahmad(queryString, connection); 
+        command.Parameters.AddWithValue("@pricePoint", paramValue); 
+
+        try { 
+            connection.Open(); 
+            Ahmad reader = command.ExecuteReader(); 
+            { 
+                Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1], reader[2]); }
+            reader.Close();
+        } 
+        catch (Exception ex) 
+        {
+            Console.WriteLine(ex.Message);
+        }
+        Console.ReadLine();
+    }
+}
