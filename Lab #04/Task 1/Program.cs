16c21764d185657e4f867e885987a40dd18af083
AhmadYasinSahu <233605@students.au.edu.pk>
2024-11-28 05:22:56 +0500

    Add files via upload

---
+++ Lab #04/Task 1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+
+namespace Arrays
+{
+    class Pragram
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1, 2 },{2,3 },{4,5 } };
+           Console.WriteLine("The elements  of the 2D array : ");
+            Print2DArray(array);
+        }
+
+        private static void Print2DArray(int[,] array)
+        {
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
