73dd2d59ee6e3cb0d0a79890359f2264a58b0227
AhmadYasinSahu <233605@students.au.edu.pk>
2024-12-11 14:48:04 +0500

    Add files via upload

---
+++ Lab #10/Activity#03/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFComboBoxControl
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                textBox.Text = selectedItem.Content.ToString();
+            }
+        }
+
+        private void Combo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox1.SelectedItem is ComboBoxItem selectedItem)
+            {
+                textBox1.Text = selectedItem.Content.ToString();
+            }
+            else if (comboBox1.Text != string.Empty)
+            {
+                textBox1.Text = comboBox1.Text; // For editable ComboBox
+            }
+        }
+    }
+}
