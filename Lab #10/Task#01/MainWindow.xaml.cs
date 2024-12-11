0f8d44dd345c209216e53d6d0955f8b6be572b5c
AhmadYasinSahu <233605@students.au.edu.pk>
2024-12-11 14:49:37 +0500

    Add files via upload

---
+++ Lab #10/Task#01/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Task01
+{
+    public partial class BringBackPocoyo : Window
+    {
+        public BringBackPocoyo()
+        {
+            InitializeComponent();
+
+            // Set image sources
+            Image1.Source = new BitmapImage(new Uri("pack://application:,,,/image1.png"));
+            Image2.Source = new BitmapImage(new Uri("pack://application:,,,/image2.png"));
+            Image3.Source = new BitmapImage(new Uri("pack://application:,,,/image3.png"));
+        }
+    }
+}
