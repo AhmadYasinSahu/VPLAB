0dd851638691d892f6e4810089892f12f400d940
AhmadYasinSahu <233605@students.au.edu.pk>
2025-01-11 15:57:16 +0500

    Add files via upload

---
+++ Lab Final/Task2/Task-2/Task-2/Class.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_2
+{
+    public class server
+    {
+        public string Name { get; set; }
+        public string Gategory { get; set; }
+        public float Price { get; set; }
+
+        server(string name, string gategory, float price)
+        {
+            Name = name;
+            Gategory = gategory;
+            Price = price;
+        }
+    }
+}
